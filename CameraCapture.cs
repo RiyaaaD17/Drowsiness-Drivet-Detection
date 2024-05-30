fe8957250a7d19c3f1846ef7ef0c9525539a0e54
Riya Deohare <80644308+RiyaaaD17@users.noreply.github.com>
2024-05-30 20:20:35 +0530

    Add files via upload

---
+++ CameraCapture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace DrowsyDoc
+{
+    class CameraCapture
+    {
+       
+
+        static VideoCapture capture;
+        static Mat frame;
+        static Bitmap image;
+        private static Thread camera;
+        static bool isCameraRunning = true;
+
+
+        // Declare required methods
+        public void CaptureCamera()
+        {
+            
+            try
+            {
+                CaptureCameraCallback();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        //Capturing Images Using The Device's Camera
+        private static void CaptureCameraCallback()
+        {
+            capture = new VideoCapture(0);
+            capture.Open(0);
+            frame = new Mat();
+
+            if (capture.IsOpened())
+            {
+                string name = Environment.CurrentDirectory;
+                int i = 0;
+
+                while (isCameraRunning)
+                {
+                    i %= 100;
+                    capture.Read(frame);
+                    image = BitmapConverter.ToBitmap(frame);
+                    Bitmap snapshot = new Bitmap(image);
+                    
+                    snapshot.Save(name + @"\rawImage\raw" + i + @".png");
+                    ++i;
+                    Thread t = Thread.CurrentThread;
+                    
+                    Thread.Sleep(1500);
+                    
+                }
+                
+            }
+        }
+        
+    }
+}
